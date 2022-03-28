11be94e2eb35824f4d5ad0c40ddad6b86bb5644b
Yusuke Hosonuma <tobi462@gmail.com>
2022-03-28 17:17:18 +0900

    1st UI

--- Root/Sources/Root/RootView.swift
+++ Root/Sources/Root/RootView.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import SFReadableSymbols
 
 public struct RootView: View {
+    @State var sourceText: String = ""
+    @State var destinationText: String = ""
+
     public init() {}
-    
+
     public var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            //
+            // Source
+            //
+            Text("Source:")
+            TextEdit("Please paste source code.", text: $sourceText, font: editorFont)
+                .padding(.bottom)
+
+            //
+            // Destination
+            //
+            HStack {
+                Text("Destination:")
+                Spacer()
+                Button {
+                    // TODO:
+                } label: {
+                    Label("Copy to Clipboard", symbol: "􀉄")
+                }
+            }
+            TextEdit(text: $destinationText, font: editorFont)
+        }
+        .padding()
+    }
+    
+    private var editorFont: Font {
+        .custom("SF Mono", size: 16)
     }
 }
