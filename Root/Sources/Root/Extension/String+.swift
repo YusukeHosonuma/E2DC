f7ae81d88c6296c8faba25365c3aa15c55fab862
Yusuke Hosonuma <tobi462@gmail.com>
2022-03-31 12:06:51 +0900

    refactor: and improve

--- Root/Sources/Root/Extension/String+.swift
+++ Root/Sources/Root/Extension/String+.swift
@@ -9,22 +9,38 @@ import Foundation
 
 extension String {
     func extractEnglishText() -> String {
-        //
-        // 😇 TODO: I would like someone to introduce a parser.
-        //
-        split(separator: "\n")
-            .map { $0.drop { $0 == " " } }
-            .joined(separator: "\n")
-            .components(separatedBy: "///\n")
+        return components(separatedBy: "\n")
             .map {
-                $0.split(separator: "\n").map {
-                    $0.replacingOccurrences(of: "/// ", with: "")
-                }
-                .joined(separator: " ")
+                $0.drop { $0 == " " }
+                    .replacingOccurrences(of: "/// ", with: "")
+                    .replacingOccurrences(of: "///", with: "")
             }
-            .joined(separator: "\n\n")
+            .reduce([String]()) {
+                if $1.isEmpty {
+                    return $0 + ["\n\n"]
+                } else if $0.last == "\n\n" {
+                    return $0 + [$1]
+                } else if $1.isListLine {
+                    return $0 + ["\n" + $1]
+                } else {
+                    return $0 + [" " + $1.trimed()]
+                }
+            }
+            .joined()
             .replacingOccurrences(of: "``", with: "`")
-            .replacingOccurrences(of: " -", with: "\n-")
-            .replacingOccurrences(of: "  \n-", with: "\n  -")
+            .trimed()
+    }
+
+    // e.g.
+    // ```
+    // - Parameters:
+    //   - start: A valid index of the collection.
+    // ```
+    private var isListLine: Bool {
+        drop { $0 == " " }.first == "-"
+    }
+    
+    private func trimed() -> String {
+        trimmingCharacters(in: .whitespaces)
     }
 }
